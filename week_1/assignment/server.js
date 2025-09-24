478487bb90d9c5e581e0c9c654d190d65b5822b1
Farisa Hilmi <farisahilmiexe@gmail.com>
2025-09-24 12:42:10 +0700

    siap publish

---
+++ week_1/assignment/server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"content-type": "text/plain"});
+    res.end("hello dari node.js server!");
+});
+
+server.listen(3000, () => {
+    console.log("server jalan di http://localhost:3000")
+})
