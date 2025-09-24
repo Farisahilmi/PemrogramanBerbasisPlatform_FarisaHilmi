452192fa53775bff382a2a89db98cbfcbff74844
Farisa Hilmi <farisahilmiexe@gmail.com>
2025-09-24 12:17:26 +0700

    commit yang baru perubahan week-1

--- week_1/assignment/index.js
+++ week_1/assignment/index.js
@@ -1,8 +1,1 @@
-const moment = require('moment');
-
-const now = moment();
-console.log("Today in string");
-console.log(now.toString());
-
-console.log("Today in YYYY-MM-DD HH:mm:ss format");
-console.log(now.format('YYYY-MM-DD HH:mm:ss'));
+console.log("Halo, Node.js jalan!");
